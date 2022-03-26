2192c1c49610870f69b9ff74ba48af5356dcaa75
Brian Huynh <vuduchv@gmail.com>
2022-03-26 13:08:40 +1100

    Rails Task Manager Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  Task.create(title: Faker::Artist.name, details: Faker::Job.title)
+end
